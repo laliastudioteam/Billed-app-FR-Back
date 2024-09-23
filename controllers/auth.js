36ad952c175895c28968b8a8ba092c2c1dde1bc3
LaliaStudio <laliastudio@gmail.com>
2024-09-23 16:15:49 +0200

    first

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,6 +4,7 @@ const jwt = require('../services/jwt');
 
 const login = async (req, res) => {
   const { password, email } = req.body;
+  console.log(password,+"/"+ email);
   try {
     const user = await User.findOne({ where: { email } });
     if (!user) return res.status(400).send({ message: 'Cannot authenticate user' });
